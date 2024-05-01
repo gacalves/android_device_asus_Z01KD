1830069ac6a0107d45f3da911a9cbd069339df2d
gacalves <geovanealvesdecarvalho@gmail.com>
2024-05-01 18:33:30 -0300

    Add board config

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm660-common
+include device/asus/sdm660-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/asus/Z01KD
+
+# Assert
+# TODO: VERIFICAR SE É PRECISO
+#TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := ASUS_Z01KD,Z01KD,Z01K
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm636
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# TODO: ANALISAR O QUE É ISTO.
+# Kernel
+TARGET_KERNEL_CONFIG := Z01KD_defconfig
+
+#TODO: ANALISAR DE ONDE VIRÁ
+# Inherit the proprietary files
+include vendor/asus/Z01KD/BoardConfigVendor.mk
